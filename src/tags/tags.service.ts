c1bc5a3848d2dd394b9c2cb619424b6fa50d65cb
xinghe98 <lcg9899@gmail.com>
2024-10-31 21:26:01 +0800

    v

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -5,46 +5,46 @@ import { PrismaService } from "nestjs-prisma";
 
 @Injectable()
 export class TagsService {
-	constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-	async create(createTagDto: CreateTagDto) {
-		const findtag = await this.prisma.tag.findFirst({
-			where: {
-				name: createTagDto.name,
-			},
-		});
-		if (findtag) {
-			throw new BadRequestException("该标签已存在");
-		}
-		const tag = await this.prisma.tag.create({ data: createTagDto });
-		return tag;
-	}
+  async create(createTagDto: CreateTagDto) {
+    const findtag = await this.prisma.tag.findFirst({
+      where: {
+        name: createTagDto.name,
+      },
+    });
+    if (findtag) {
+      throw new BadRequestException("该标签已存在");
+    }
+    const tag = await this.prisma.tag.create({ data: createTagDto });
+    return tag;
+  }
 
-	async findAll() {
-		return await this.prisma.tag.findMany();
-	}
+  async findAll() {
+    return await this.prisma.tag.findMany();
+  }
 
-	async findOne(id: number) {
-		const findtag = await this.prisma.tag.findMany({
-			where: { id: id },
-			include: { articles: true },
-		});
-		if (!findtag) {
-			throw new BadRequestException("该标签不存在");
-		}
-		return findtag;
-	}
+  async findOne(id: number) {
+    const findtag = await this.prisma.tag.findMany({
+      where: { id: id },
+      include: { articles: true },
+    });
+    if (!findtag) {
+      throw new BadRequestException("该标签不存在");
+    }
+    return findtag;
+  }
 
-	async update(id: number, updateTagDto: UpdateTagDto) {
-		await this.prisma.tag.update({ where: { id: id }, data: updateTagDto });
-		return { name: updateTagDto.name };
-	}
+  async update(id: number, updateTagDto: UpdateTagDto) {
+    await this.prisma.tag.update({ where: { id: id }, data: updateTagDto });
+    return { name: updateTagDto.name };
+  }
 
-	async remove(id: number) {
-		const tag = await this.prisma.tag.findUnique({ where: { id: id } });
-		if (!tag) {
-			throw new BadRequestException("该标签不存在");
-		}
-		await this.prisma.tag.delete({ where: { id: id } });
-	}
+  async remove(id: number) {
+    const tag = await this.prisma.tag.findUnique({ where: { id: id } });
+    if (!tag) {
+      throw new BadRequestException("该标签不存在");
+    }
+    await this.prisma.tag.delete({ where: { id: id } });
+  }
 }
